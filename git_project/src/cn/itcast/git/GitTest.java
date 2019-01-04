b9a2a8496fc69c514cbb1c4fda2118c6a0e8bc82
yanji <1129712834@qq.com>
2019-01-04 10:53:15 +0800

    把本地工程添加到git

---
+++ git_project/src/cn/itcast/git/GitTest.java
@@ -0,0 +1,9 @@
+package cn.itcast.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好！");
+        System.out.println("他好！");
+        System.out.println("我也好！");
+    }
+}
