1e531ff10a4bcc3dc8a93e10d393cb1af2356a6c
yanji <1129712834@qq.com>
2019-01-04 11:13:26 +0800

    克隆下来的代码修改

--- git_project/src/cn/itcast/git/GitTest.java
+++ git_project/src/cn/itcast/git/GitTest.java
@@ -5,9 +5,11 @@ public class GitTest {
         System.out.println("你好！");
         System.out.println("他好！");
         System.out.println("我也好！");
+        System.out.println("我是克隆出来的");
     }
     public void test(){
         int i = 1;
         i += 3;
+        System.out.println(i);
     }
 }
